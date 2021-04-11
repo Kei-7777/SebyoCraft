0329c7138399dce194afce58357fbcee6ecf5ec1
Kei-7777 <69491052+Kei-7777@users.noreply.github.com>
2021-04-11 17:46:41 +0900

    誰とでもセックスができるように。

--- src/main/java/com/github/kei7777/sebyocraft/listener/ToggleSneakListener.java
+++ src/main/java/com/github/kei7777/sebyocraft/listener/ToggleSneakListener.java
@@ -7,76 +7,49 @@ import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+
 import java.util.*;
 
 public class ToggleSneakListener implements Listener {
-    public static Map<String, Integer> data;
     SebyoCraft sc;
+
     public ToggleSneakListener(SebyoCraft sebyoCraft) {
-        data = new HashMap<>();
         sc = sebyoCraft;
-        for (String s : sebyoCraft.getConfig().getStringList("users")) {
-            data.put(s, 0);
-        }
     }
 
     @EventHandler
-    public void on(PlayerToggleSneakEvent e){
-        if(!e.isSneaking()) return;
-        if (!data.containsKey(e.getPlayer().getName())) {
-            Player p = e.getPlayer();
-            for (Entity entity : p.getNearbyEntities(1, 1, 1)) {
-                if (entity instanceof Player) {
-                    Player target = ((Player) entity);
-                    if (data.containsKey(target.getName())) {
-                        boolean rub = (target.getInventory().getItemInMainHand().isSimilar(SebyoItemFactory.rub) || p.getInventory().getItemInMainHand().isSimilar(SebyoItemFactory.rub)); // コンドームの有無
-                        int o = data.get(target.getName()) + 1;
-
-                        for (Player pl : Bukkit.getOnlinePlayers()) {
-                            pl.playSound(p.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1f, 1f);
-                        }
-
-                        p.getLocation().getWorld().spawnParticle(Particle.HEART, p.getLocation().clone().add(0, 2.5, 0), 3);
-                        p.getLocation().getWorld().spawnParticle(Particle.HEART, target.getLocation().clone().add(0, 2.5, 0), 3);
-
-                        if (o >= sc.getConfig().getInt(target.getName())) {
-                            data.replace(target.getName(), 0);
-
-                            Location loc = target.getLocation();
-                            Collections.shuffle(sc.mobs);
-                            String str = rub ? "てしまった！" : "た！";
-                            if(rub){
-                                if(target.getInventory().getItemInMainHand().isSimilar(SebyoItemFactory.rub)) {
-                                    if(target.getInventory().getItemInMainHand().getAmount() != 0) {
-                                        target.getInventory().getItemInMainHand().setAmount(target.getInventory().getItemInMainHand().getAmount() - 1);
-                                    } else {
-                                        target.getInventory().getItemInMainHand().setType(Material.AIR);
-                                    }
-                                } else {
-                                    if(p.getInventory().getItemInMainHand().getAmount() != 0) {
-                                        p.getInventory().getItemInMainHand().setAmount(p.getInventory().getItemInMainHand().getAmount() - 1);
-                                    } else {
-                                        p.getInventory().getItemInMainHand().setType(Material.AIR);
-                                    }
-                                }
-                            }
-                            loc.getWorld().spawnEntity(loc, sc.mobs.get(0)).setCustomName(ChatColor.LIGHT_PURPLE + target.getName() + ChatColor.WHITE + " x " + ChatColor.AQUA + p.getName() + ChatColor.WHITE + " の子供");
-                            Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + target.getName() + ChatColor.WHITE + "と" + ChatColor.AQUA + p.getName() + ChatColor.WHITE + "の間に" + ChatColor.GOLD + sc.mobs.get(0).name() + ChatColor.WHITE + "が産まれ" +
-                                    str);
-                            for (Player pl : Bukkit.getOnlinePlayers()) {
-                                pl.playSound(p.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
-                            }
-
-                            if(!rub){
-                                IllnessManager.mix(target, p);
-                                IllnessManager.check(target);
-                                IllnessManager.check(p);
-                            }
-
+    public void on(PlayerToggleSneakEvent e) {
+        if (!e.isSneaking()) return;
+        Player p = e.getPlayer();
+        for (Entity entity : p.getNearbyEntities(1, 1, 1)) {
+            if (entity instanceof Player && !entity.getUniqueId().equals(p.getUniqueId())) {
+                Player target = ((Player) entity);
+                boolean rub = (target.getInventory().getItemInMainHand().isSimilar(SebyoItemFactory.rub) || p.getInventory().getItemInMainHand().isSimilar(SebyoItemFactory.rub)); // コンドームの有無
+                for (Player pl : Bukkit.getOnlinePlayers()) {
+                    pl.playSound(p.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1f, 1f);
+                }
+                p.getLocation().getWorld().spawnParticle(Particle.HEART, p.getLocation().clone().add(0, 2.5, 0), 3);
+                p.getLocation().getWorld().spawnParticle(Particle.HEART, target.getLocation().clone().add(0, 2.5, 0), 3);
+                Location loc = target.getLocation();
+                if (rub) {
+                    if (target.getInventory().getItemInMainHand().isSimilar(SebyoItemFactory.rub)) {
+                        if (target.getInventory().getItemInMainHand().getAmount() != 0) {
+                            target.getInventory().getItemInMainHand().setAmount(target.getInventory().getItemInMainHand().getAmount() - 1);
                         } else {
-                            data.replace(target.getName(), o);
+                            target.getInventory().getItemInMainHand().setType(Material.AIR);
+                        }
+                    } else {
+                        if (p.getInventory().getItemInMainHand().getAmount() != 0) {
+                            p.getInventory().getItemInMainHand().setAmount(p.getInventory().getItemInMainHand().getAmount() - 1);
+                        } else {
+                            p.getInventory().getItemInMainHand().setType(Material.AIR);
                         }
                     }
+                }
+                if (!rub) {
+                    IllnessManager.mix(target, p);
+                    IllnessManager.check(target);
+                    IllnessManager.check(p);
                 }
             }
         }
